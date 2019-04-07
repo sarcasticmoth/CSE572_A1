2b0f673bd6f315359ce3600ba03b3c2ae7196e9f
Vanessa Ulloa <vanessa.e.ulloa@gmail.com>
2019-04-07 12:47:05 -0700

    updated to include all data for 6 users

---
+++ Phase1.m
@@ -0,0 +1,296 @@
+clear all
+clc
+
+% file variables
+workingDir = 'C:\Users\rando\OneDrive\Graduate School\SPRING 2019\CSE572_Data_Mining\CSE572_A1\';
+dataPath = fullfile(workingDir, 'Data', filesep);
+myoDataPath = fullfile(dataPath, 'MyoData', filesep);
+gtDataPath = fullfile(dataPath, 'groundTruth', filesep);
+endPath = fullfile(workingDir, 'Result', filesep);
+
+% remove folders . and ..
+list = dir(gtDataPath);
+list = list(~ismember({list.name},{'.','..'}));
+
+% predefined variables
+imu_EA=[];
+% emg_EA=[];
+imu_NEA=[];
+emg_nNEA=[];
+
+% for each folder in the Data folder
+% should return 6 users/folders
+for i = 1:size(list, 1)
+    % get the user fork path
+    forkPath = fullfile(gtDataPath, list(i,:).name, "fork");
+    
+    % get the list of files in the folder
+    forklist = dir(forkPath);
+    forklist = forklist(~ismember({forklist.name},{'.','..'}));
+    
+    % read the ground truth file for this user
+    gtFileData = dlmread(fullfile(strtrim(forklist(1,:).folder), strtrim(forklist(1,:).name)));
+    
+    % get my myoData for fork
+    myo = dir(myoDataPath);
+    myo = myo(~ismember({myo.name},{'.','..'}));
+    
+    myoFork = fullfile(strtrim(myo(i,:).folder), strtrim(myo(i,:).name), "fork");
+    myoFolders = dir(myoFork);
+    myoFolders = myoFolders(~ismember({myoFolders.name},{'.','..'}));
+    
+    % get eating action of fork for this user %
+    for j = 1:size(myoFolders,1)
+        if contains(myoFolders(j,:).name, "IMU")
+            IMUfile = dlmread(fullfile(strtrim(myoFolders(j,:).folder), strtrim(myoFolders(j,:).name)));
+        end
+        
+        if contains(myoFolders(j,:).name, "EMG")
+            EMGfile = dlmread(fullfile(strtrim(myoFolders(j,:).folder), strtrim(myoFolders(j,:).name)));
+        end
+    end
+    
+    % for the eating data
+    % we synchornize the frames where an eating action occurs
+    % with the IMU and EMG data.
+    % data outside of these start and end frames are non-eating
+    for j = 1: size(gtFileData, 1)
+        % convert frames to sample numbers
+        
+        % for IMU data it is 50 Hz and 30 FPS
+        s = floor(gtFileData(j, 1) * (50/30));
+        e = floor(gtFileData(j, 2) * (50/30));
+        
+        found = IMUfile(s:e, 2:size(IMUfile, 2));
+        
+        % compare the found rows to the matrix columns
+        % since the data is transposed
+        if size(found, 1) > size(imu_EA, 2) && j > 1
+            a = size(imu_EA, 1);
+            b = size(imu_EA, 2);
+            c = size(found, 1);
+            d = size(found, 2);
+            
+            newCols = c;
+            newRows = a + d;
+            
+            temp = zeros(newRows, newCols);
+            temp(1:a, 1:b) = imu_EA;
+            
+            imu_EA = temp;
+            
+            imu_EA((a+1):newRows, 1:newCols) = found';
+        else
+            if size(found, 1) < size(imu_EA, 2)
+                x = found';
+                x = [x zeros(size(x, 1), size(imu_EA, 2) - size(x, 2))];
+                imu_EA = [imu_EA; x];
+            else
+                imu_EA = [imu_EA; found'];
+            end
+        end        
+    end
+    
+    % for the non-eating data
+    % we synchornize the frames where a non-eating action occurs
+    % with the IMU and EMG data.
+    % the data is located between an end frame and the next start frame
+    for j = 2:size(gtFileData, 1)
+        % convert frames to sample numbers
+        
+        % for IMU data it is 50 Hz and 30 FPS
+        s = floor(gtFileData(j - 1, 2) * (50/30));
+        e = floor(gtFileData(j, 1) * (50/30));
+        
+        found = IMUfile(s:e, 2:size(IMUfile, 2));
+        
+        % compare the found rows to the matrix columns
+        % since the data is transposed
+        if size(found, 1) > size(imu_NEA, 2) && j > 1
+            a = size(imu_NEA, 1);
+            b = size(imu_NEA, 2);
+            c = size(found, 1);
+            d = size(found, 2);
+            
+            newCols = c;
+            newRows = a + d;
+            
+            temp = zeros(newRows, newCols);
+            temp(1:a, 1:b) = imu_NEA;
+            
+            imu_NEA = temp;
+            
+            imu_NEA((a+1):newRows, 1:newCols) = found';
+        else
+            if size(found, 1) < size(imu_NEA, 2)
+                x = found';
+                x = [x zeros(size(x, 1), size(imu_NEA, 2) - size(x, 2))];
+                imu_NEA = [imu_NEA; x];
+            else
+                imu_NEA = [imu_NEA; found'];
+            end
+        end
+    end
+end
+
+disp(size(imu_EA));
+disp(size(imu_NEA));
+
+% matrices are organized in this way:
+% action OriX
+% action OriY
+% action OriZ
+% action OriW
+% action AccX
+% action AccY
+% action AccZ
+% action GyroX
+% action GyroY
+% action GyroZ
+
+% the number of rows in each matrix divided by the total
+% number of sensors gives estimate of number of actions
+sensors = 10;
+EA_actions = size(imu_EA, 1) / sensors;
+NEA_actions = size(imu_NEA, 1) / sensors;
+
+disp(EA_actions);
+disp(NEA_actions);
+
+% save files
+
+
+totalFrames = size(vidFile, 1);
+eatingAction = 1;
+
+OriX=[];
+OriY=[];
+OriZ=[];
+OriW=[];
+AccX=[];
+AccY=[];
+AccZ=[];
+GyrX=[];
+GyrY=[];
+GyrZ=[];
+
+EAS={};
+NEAS={};
+
+for i = 1:totalFrames          
+%     OriX = [OriX; {strcat('Eating Action',num2str(eatingAction)),'OriX'}, pIMU(floor(vidFile(i,1)*(50/30)):floor(vidFile(i,2)*(50/30)), 2)'];
+%     OriY = [OriY; {strcat('Eating Action',num2str(eatingAction)),'OriY'}, pIMU(floor(vidFile(i,1)*(50/30)):floor(vidFile(i,2)*(50/30)), 3)'];
+%     OriZ = [OriZ; {strcat('Eating Action',num2str(eatingAction)),'OriZ'}, pIMU(floor(vidFile(i,1)*(50/30)):floor(vidFile(i,2)*(50/30)), 4)'];
+%     OriW = [OriW; {strcat('Eating Action',num2str(eatingAction)),'OriW'}, pIMU(floor(vidFile(i,1)*(50/30)):floor(vidFile(i,2)*(50/30)), 5)'];
+%     AccX = [AccX; {strcat('Eating Action',num2str(eatingAction)),'AccX'}, pIMU(floor(vidFile(i,1)*(50/30)):floor(vidFile(i,2)*(50/30)), 6)'];
+%     AccY = [AccY; {strcat('Eating Action',num2str(eatingAction)),'AccY'}, pIMU(floor(vidFile(i,1)*(50/30)):floor(vidFile(i,2)*(50/30)), 7)'];
+%     AccZ = [AccZ; {strcat('Eating Action',num2str(eatingAction)),'AccZ'}, pIMU(floor(vidFile(i,1)*(50/30)):floor(vidFile(i,2)*(50/30)), 8)'];
+%     GyrX = [GyrX; {strcat('Eating Action',num2str(eatingAction)),'GyrX'}, pIMU(floor(vidFile(i,1)*(50/30)):floor(vidFile(i,2)*(50/30)), 9)'];
+%     GyrY = [GyrY; {strcat('Eating Action',num2str(eatingAction)),'GyrY'}, pIMU(floor(vidFile(i,1)*(50/30)):floor(vidFile(i,2)*(50/30)), 10)'];
+%     GyrZ = [GyrZ; {strcat('Eating Action',num2str(eatingAction)),'GyrZ'}, pIMU(floor(vidFile(i,1)*(50/30)):floor(vidFile(i,2)*(50/30)), 11)'];
+    
+    OriX = [OriX pIMU(floor(vidFile(i,1)*(50/30)):floor(vidFile(i,2)*(50/30)), 2)'];
+    OriY = [OriY pIMU(floor(vidFile(i,1)*(50/30)):floor(vidFile(i,2)*(50/30)), 3)'];
+    OriZ = [OriZ pIMU(floor(vidFile(i,1)*(50/30)):floor(vidFile(i,2)*(50/30)), 4)'];
+    OriW = [OriW pIMU(floor(vidFile(i,1)*(50/30)):floor(vidFile(i,2)*(50/30)), 5)'];
+    AccX = [AccX pIMU(floor(vidFile(i,1)*(50/30)):floor(vidFile(i,2)*(50/30)), 6)'];
+    AccY = [AccY pIMU(floor(vidFile(i,1)*(50/30)):floor(vidFile(i,2)*(50/30)), 7)'];
+    AccZ = [AccZ pIMU(floor(vidFile(i,1)*(50/30)):floor(vidFile(i,2)*(50/30)), 8)'];
+    GyrX = [GyrX pIMU(floor(vidFile(i,1)*(50/30)):floor(vidFile(i,2)*(50/30)), 9)'];
+    GyrY = [GyrY pIMU(floor(vidFile(i,1)*(50/30)):floor(vidFile(i,2)*(50/30)), 10)'];
+    GyrZ = [GyrZ pIMU(floor(vidFile(i,1)*(50/30)):floor(vidFile(i,2)*(50/30)), 11)']; 
+    
+%     EA = [EA; OriX; OriY; OriZ; OriW; AccX; AccY; AccZ; GyrX; GyrY; GyrZ;]
+%     EA = [EA OriX OriY OriZ OriW AccX AccY AccZ GyrX GyrY GyrZ]
+	EA = {strcat('Eating Action',num2str(eatingAction)), OriX, OriY, OriZ, OriW, AccX, AccY, AccZ, GyrX, GyrY, GyrZ};
+
+    save(fullfile(output, list(i, :) + "_" + 'test.csv'), 'EA');
+    
+    EAS = [EAS; EA];
+    
+    % reset variables
+    eatingAction = eatingAction + 1;
+    
+    OriX=[];
+    OriY=[];
+    OriZ=[];
+    OriW=[];
+    AccX=[];
+    AccY=[];
+    AccZ=[];
+    GyrX=[];
+    GyrY=[];
+    GyrZ=[];
+
+end
+
+eatingAction = 1;
+
+% this is the table of Non Eating actions and the IMU data
+EAT = cell2table(EAS, 'VariableNames', {'EA', 'OriX', 'OriY', 'OriZ', 'OriW', 'AccX', 'AccY', 'AccZ', 'GyrX', 'GyrY', 'GyrZ'});
+% writetable(EAT, '1503512024740_EatingActions_Table.csv');
+% writecell(EAS, '1503512024740_EatingActions.csv');
+
+% non-eating actions:
+nonEatingAction = 1;
+
+for i = 2:totalFrames       
+%     OriX = [OriX; {strcat('Non-Eating Action',num2str(nonEatingAction)),'OriX'}, pIMU(floor(vidFile(i,1)*(50/30)):floor(vidFile(i,2)*(50/30)), 2)'];
+%     OriY = [OriY; {strcat('Non-Eating Action',num2str(nonEatingAction)),'OriY'}, pIMU(floor(vidFile(i,1)*(50/30)):floor(vidFile(i,2)*(50/30)), 3)'];
+%     OriZ = [OriZ; {strcat('Non-Eating Action',num2str(nonEatingAction)),'OriZ'}, pIMU(floor(vidFile(i,1)*(50/30)):floor(vidFile(i,2)*(50/30)), 4)'];
+%     OriW = [OriW; {strcat('Non-Eating Action',num2str(nonEatingAction)),'OriW'}, pIMU(floor(vidFile(i,1)*(50/30)):floor(vidFile(i,2)*(50/30)), 5)'];
+%     AccX = [AccX; {strcat('Non-Eating Action',num2str(nonEatingAction)),'AccX'}, pIMU(floor(vidFile(i,1)*(50/30)):floor(vidFile(i,2)*(50/30)), 6)'];
+%     AccY = [AccY; {strcat('Non-Eating Action',num2str(nonEatingAction)),'AccY'}, pIMU(floor(vidFile(i,1)*(50/30)):floor(vidFile(i,2)*(50/30)), 7)'];
+%     AccZ = [AccZ; {strcat('Non-Eating Action',num2str(nonEatingAction)),'AccZ'}, pIMU(floor(vidFile(i,1)*(50/30)):floor(vidFile(i,2)*(50/30)), 8)'];
+%     GyrX = [GyrX; {strcat('Non-Eating Action',num2str(nonEatingAction)),'GyrX'}, pIMU(floor(vidFile(i,1)*(50/30)):floor(vidFile(i,2)*(50/30)), 9)'];
+%     GyrY = [GyrY; {strcat('Non-Eating Action',num2str(nonEatingAction)),'GyrY'}, pIMU(floor(vidFile(i,1)*(50/30)):floor(vidFile(i,2)*(50/30)), 10)'];
+%     GyrZ = [GyrZ; {strcat('Non-Eating Action',num2str(nonEatingAction)),'GyrZ'}, pIMU(floor(vidFile(i,1)*(50/30)):floor(vidFile(i,2)*(50/30)), 11)'];
+    
+    OriX = [OriX pIMU(floor(vidFile(i,1)*(50/30)):floor(vidFile(i,2)*(50/30)), 2)'];
+    OriY = [OriY pIMU(floor(vidFile(i,1)*(50/30)):floor(vidFile(i,2)*(50/30)), 3)'];
+    OriZ = [OriZ pIMU(floor(vidFile(i,1)*(50/30)):floor(vidFile(i,2)*(50/30)), 4)'];
+    OriW = [OriW pIMU(floor(vidFile(i,1)*(50/30)):floor(vidFile(i,2)*(50/30)), 5)'];
+    AccX = [AccX pIMU(floor(vidFile(i,1)*(50/30)):floor(vidFile(i,2)*(50/30)), 6)'];
+    AccY = [AccY pIMU(floor(vidFile(i,1)*(50/30)):floor(vidFile(i,2)*(50/30)), 7)'];
+    AccZ = [AccZ pIMU(floor(vidFile(i,1)*(50/30)):floor(vidFile(i,2)*(50/30)), 8)'];
+    GyrX = [GyrX pIMU(floor(vidFile(i,1)*(50/30)):floor(vidFile(i,2)*(50/30)), 9)'];
+    GyrY = [GyrY pIMU(floor(vidFile(i,1)*(50/30)):floor(vidFile(i,2)*(50/30)), 10)'];
+    GyrZ = [GyrZ pIMU(floor(vidFile(i,1)*(50/30)):floor(vidFile(i,2)*(50/30)), 11)'];
+       
+%   NEA = [EA; OriX; OriY; OriZ; OriW; AccX; AccY; AccZ; GyrX; GyrY; GyrZ;]
+%   NEA = [EA OriX OriY OriZ OriW AccX AccY AccZ GyrX GyrY GyrZ]
+	NEA = {strcat('Non Eating Action',num2str(nonEatingAction)), OriX, OriY, OriZ, OriW, AccX, AccY, AccZ, GyrX, GyrY, GyrZ};
+        
+    NEAS = [NEAS; NEA];
+    
+    % reset variables
+    nonEatingAction = nonEatingAction + 1;
+    
+    OriX=[];
+    OriY=[];
+    OriZ=[];
+    OriW=[];
+    AccX=[];
+    AccY=[];
+    AccZ=[];
+    GyrX=[];
+    GyrY=[];
+    GyrZ=[];
+
+end
+
+nonEatingAction = 1;
+
+% this is the table of Non Eating actions and the IMU data
+NEAT = cell2table(NEAS, 'VariableNames', {'EA', 'OriX', 'OriY', 'OriZ', 'OriW', 'AccX', 'AccY', 'AccZ', 'GyrX', 'GyrY', 'GyrZ'});
+% writecell(NEAS, '1503512024740_NonEatingActions.csv');
+% writetable(NEAT, '1503512024740_NonEatingActions_Table.csv');
+
+% sample, get OriX values for EA1
+x = EAT.OriX{1,1};
+y = NEAT.OriX{1,1};
+
+hold off;
+
+
+
